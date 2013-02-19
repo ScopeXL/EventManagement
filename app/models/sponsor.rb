255c7e9e4cdc112d2db1368f0691e51783281147
Ed Roesch <eroesch@iupui.edu>
2013-02-19 02:08:39 -0500

    Initial Commit

---
+++ app/models/sponsor.rb
@@ -0,0 +1,11 @@
+class Sponsor < ActiveRecord::Base
+  attr_accessible :logo, :url, :exhibitor, :meetings
+  
+  validates :logo, :presence => true
+  validates :url, :presence => true
+  validates :exhibitor, :presence => true
+  
+  has_one :exhibitor
+  has_and_belongs_to_many :meetings
+  
+end
